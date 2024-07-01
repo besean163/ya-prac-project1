4dfc05a32ce6c398479260eb755afc8520c23424
Sergey Belousov <besean163@gmail.com>
2024-07-01 22:30:57 +0400

    chore: make increment#5

--- cmd/agent/flag.go
+++ cmd/agent/flag.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 var serverEndpointFlag string
@@ -19,6 +21,24 @@ func parseFlags() {
 	flag.IntVar(&reportIntervalFlag, "r", reportInterval, "report interval sec")
 	flag.IntVar(&pollIntervalFlag, "p", poolInterval, "metrics pool interval sec")
 	flag.Parse()
+
+	if envServerAddr := os.Getenv("ADDRESS"); envServerAddr != "" {
+		serverEndpointFlag = envServerAddr
+	}
+
+	if envReportInv := os.Getenv("REPORT_INTERVAL"); envReportInv != "" {
+		interval, err := strconv.Atoi(envReportInv)
+		if err == nil {
+			reportIntervalFlag = interval
+		}
+	}
+
+	if envPoolInv := os.Getenv("POLL_INTERVAL"); envPoolInv != "" {
+		interval, err := strconv.Atoi(envPoolInv)
+		if err == nil {
+			pollIntervalFlag = interval
+		}
+	}
 
 	fmt.Println("Run with:")
 	fmt.Println("Storage server endpoint: ", serverEndpointFlag)
--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -1,10 +1,17 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var serverEndpointFlag string
 
 func parseFlags() {
 	flag.StringVar(&serverEndpointFlag, "a", "localhost:8080", "server endpoint")
 	flag.Parse()
+
+	if envServerAddr := os.Getenv("ADDRESS"); envServerAddr != "" {
+		serverEndpointFlag = envServerAddr
+	}
 }
