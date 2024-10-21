ebe388b0665879984b857b1042a71c59eedc7e4e
Sergey Belousov <besean163@gmail.com>
2024-10-21 19:53:10 +0400

    chore: make increment#16

--- internal/storage/inmemstorage/inmemstorage_test.go
+++ internal/storage/inmemstorage/inmemstorage_test.go
@@ -25,5 +25,60 @@ func TestSetMetrics(t *testing.T) {
 	assert.Equal(t, s.Metrics, []metrics.Metrics{
 		metrics.NewMetric("id1", "type1", "30"),
 	})
+}
 
+func TestGetMetrics(t *testing.T) {
+	ms := []metrics.Metrics{
+		metrics.NewMetric("test_1", metrics.MetricTypeGauge, "1.5"),
+		metrics.NewMetric("test_2", metrics.MetricTypeGauge, "2.5"),
+	}
+	s := NewStorage()
+	s.SetMetrics(ms)
+
+	assert.Equal(t, ms, s.GetMetrics())
+}
+
+func TestUpdateMetrics(t *testing.T) {
+	ms := []metrics.Metrics{
+		metrics.NewMetric("test_1", metrics.MetricTypeGauge, "1.5"),
+		metrics.NewMetric("test_2", metrics.MetricTypeGauge, "2.5"),
+	}
+
+	ums := []metrics.Metrics{
+		metrics.NewMetric("test_1", metrics.MetricTypeGauge, "10.5"),
+	}
+
+	s := NewStorage()
+	s.SetMetrics(ms)
+
+	s.UpdateMetrics(ums)
+
+	expect := []metrics.Metrics{
+		metrics.NewMetric("test_1", metrics.MetricTypeGauge, "10.5"),
+		metrics.NewMetric("test_2", metrics.MetricTypeGauge, "2.5"),
+	}
+	assert.Equal(t, expect, s.GetMetrics())
+}
+
+func TestCreateMetrics(t *testing.T) {
+	ms := []metrics.Metrics{
+		metrics.NewMetric("test_1", metrics.MetricTypeGauge, "1.5"),
+		metrics.NewMetric("test_2", metrics.MetricTypeGauge, "2.5"),
+	}
+
+	cms := []metrics.Metrics{
+		metrics.NewMetric("test_3", metrics.MetricTypeGauge, "3.5"),
+	}
+
+	s := NewStorage()
+	s.SetMetrics(ms)
+
+	s.CreateMetrics(cms)
+
+	expect := []metrics.Metrics{
+		metrics.NewMetric("test_1", metrics.MetricTypeGauge, "1.5"),
+		metrics.NewMetric("test_2", metrics.MetricTypeGauge, "2.5"),
+		metrics.NewMetric("test_3", metrics.MetricTypeGauge, "3.5"),
+	}
+	assert.Equal(t, expect, s.GetMetrics())
 }
